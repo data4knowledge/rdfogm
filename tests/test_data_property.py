2c986250faf95a9055912b03609214622b9bf25e
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2021-12-26 16:30:29 +0100

    Further refactoring and tidy up

--- tests/test_data_property.py
+++ tests/test_data_property.py
@@ -8,8 +8,3 @@ def test_setting_name():
     params = {'name': 'Jack'}
     property = DataProperty(**params)
     assert property.name == "Jack"
-
-#def test_string_literal():
-#    result = DataProperty.string_literal()
-#    assert result.datatype == URIRef('http://www.w3.org/2001/XMLSchema#string')
-    
