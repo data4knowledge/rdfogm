ed3d0c49f6ba1d9f6b21c33009902948ab4f98ed
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2021-12-26 16:44:07 +0100

    Start building

--- tests/test_rdfogm.py
+++ tests/test_rdfogm.py
@@ -1,24 +1,54 @@
 import pytest
 import rdfogm
 
+from rdflib import URIRef, Literal
 from rdfogm import Model
 from rdfogm.data_property import DataProperty
 
-def test_rdfogm():
+class Person(Model):
+    params = {'name': 'name'}
+    name = DataProperty(**params)
+    surname = DataProperty(**{'name': 'surname'})
 
-    class Person(Model):
-        params = {'name': 'name'}
-        name = DataProperty(**params)
-        surname = DataProperty(**{'name': 'surname'})
+    def __init__(self):
+        super().__init__()
 
-        def __init__(self):
-            super().__init__()
-
-
+def test_instance_no_params():
     person = Person()
-    print(person)
     person.name = "Jack"
     person.surname = "Daniel's"
     assert person.name == "Jack"
     assert person.surname == "Daniel's"
+
+def test_instance_dict_params():
+    person = Person(name="Jack", surname = "Daniel's")
+    assert person.name == "Jack"
+    assert person.surname == "Daniel's"
+
+def test_save_full():
+    person = Person()
+    person.name = "Jack"
+    person.surname = "Daniel's"
+    person.save
+
+def test_save_partial():
+    uri = URIRef("http://dbpedia.org/resource/Asturias")
+    person = Person.find(uri)
+    person.name = "Jack Updated"
+    person.save
+
+def test_find():
+    uri = URIRef("http://dbpedia.org/resource/Asturias")
+    person = Person.find(uri)
+
+def test_cannot_find():
+    uri = URIRef("http://dbpedia.org/resource/Asturias")
+    person = Person.find(uri)
+
+def test_extra_triples():
+    uri = URIRef("http://dbpedia.org/resource/Asturias")
+    person = Person.find(uri)
+
+
+
 
