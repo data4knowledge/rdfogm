0eb25b29b2805c0bf51031c2250825772ae69775
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2021-12-30 15:31:13 +0100

    Add URI methods

--- rdfogm/property_uri.py
+++ rdfogm/property_uri.py
@@ -1,3 +1,5 @@
+import base64
+
 from rdflib.term import URIRef
 
 class PropertyUri(URIRef):
@@ -5,5 +7,24 @@ class PropertyUri(URIRef):
     def __init__(self, value):
         self.__uri = URIRef(value)
 
+    def to_id(self):
+        message_bytes = self.__uri.encode('ascii')
+        base64_bytes = base64.b64encode(message_bytes)
+        return base64_bytes.decode('ascii')
+
+    @classmethod
+    def from_id(cls, id):
+        base64_bytes = id.encode('ascii')
+        message_bytes = base64.b64decode(base64_bytes)
+        return PropertyUri(message_bytes.decode('ascii'))
+
     def __str__(self):
         return self.__uri.__str__()
+
+
+
+
+message = "Python is fun"
+message_bytes = message.encode('ascii')
+base64_bytes = base64.b64encode(message_bytes)
+base64_message = base64_bytes.decode('ascii')
--- tests/test_property_uri.py
+++ tests/test_property_uri.py
@@ -9,3 +9,11 @@ def test_initial_state():
 def test_initial_state_empty():
     value = PropertyUri("")
     assert value.__str__() == ""
+
+def test_to_id():
+    value = PropertyUri("http://www.example.com#A").to_id()
+    assert value == "aHR0cDovL3d3dy5leGFtcGxlLmNvbSNB"
+    
+def test_from_id():
+    value = PropertyUri.from_id("aHR0cDovL3d3dy5leGFtcGxlLmNvbSNB")
+    assert value == PropertyUri("http://www.example.com#A")
